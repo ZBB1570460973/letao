7a8a5ed400921a84c39fd71b14d1c8291f4b6e90
zhaobb <1570460973@qq.com>
2018-08-08 16:27:23 +0800

    首页公共部分完成

--- public/my-back/js/common.js
+++ public/my-back/js/common.js
@@ -23,3 +23,51 @@ $(document).ajaxStop(function () {
     NProgress.done();
   }, 1000)
 })
+$(function () {
+  // 1.点击按钮侧边栏二级菜单显示和隐藏
+  $('.category').click(function () {
+    $('.lt-aside .child').stop().slideToggle();
+  })
+  // 2.点击切换图标按钮,左边的侧边栏向左边慢慢消失,右边慢慢向左边滑动
+  $('.icon-menu').click(function () {
+    $('.lt-aside').toggleClass('hiddenMenu');
+    $('.lt-main .topbar').toggleClass('hiddenMenu');
+    $('.lt-main').toggleClass('hiddenMenu');
+  })
+  // 3.给退出按钮注册点击事件,当点击时,弹出模态框
+  $('.icon-logout').click(function() {
+    $('#logoutModal').modal('show')
+  })
+  // 4.点击退出按钮,退出模态框,重新发送ajax请求,退回到登录页面
+  $('.logoutBtn').click(function () {
+    $.ajax({
+      url: "/employee/employeeLogout",
+      type: "get",
+      dataType: "json",
+      success: function (info) {
+        // console.log(info);
+        if (info.success) {
+          location.href = "login.html"
+        }
+      }
+    })
+  })
+})
+// 5.判断用户是否登录过,如果登录过,可以浏览,如果没有,要重新登录
+// 如果一开始在的页面不是登录页面,也要登录
+  if (location.href.indexOf("login.html") == -1) {
+    $.ajax({
+      type: "get",
+      url: "/employee/checkRootLogin",
+      dataType: "json",
+      success: function (info) {
+        // console.log(info);
+        if (info.success) {
+          console.log('已登陆')
+        }
+        if (info.error === 400) {
+          location.href = "login.html"
+        }
+      }
+    })
+  }
